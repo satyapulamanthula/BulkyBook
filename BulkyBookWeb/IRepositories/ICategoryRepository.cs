0ffb8fb265ca5cc7a81ee1053fcadfece13d9e3e
Satya Pulamanthula <satya.pulamanthula@ecolab.com>
2023-10-31 14:35:45 +0530

    Add project files.

---
+++ BulkyBookWeb/IRepositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.IRepositories
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAllCategories();
+        Category GetCategoryById(int id);
+        void CreateCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(Category category);
+    }
+}
---
+++ BulkyBookWeb/Repositories/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.IRepositories;
+using BulkyBookWeb.Models;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public CategoryRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IEnumerable<Category> GetAllCategories()
+    {
+        return _dbContext.Categories;
+    }
+
+    public Category GetCategoryById(int id)
+    {
+        return _dbContext.Categories.Find(id);
+    }
+
+    public void CreateCategory(Category category)
+    {
+        _dbContext.Categories.Add(category);
+        _dbContext.SaveChanges();
+    }
+
+    public void UpdateCategory(Category category)
+    {
+        _dbContext.Categories.Update(category);
+        _dbContext.SaveChanges();
+    }
+
+    public void DeleteCategory(Category category)
+    {
+        _dbContext.Categories.Remove(category);
+        _dbContext.SaveChanges();
+    }
+}
